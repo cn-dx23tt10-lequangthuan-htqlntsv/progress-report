69c5ba57beac853e179d20722a279ae1428c482a
cn-dx23tt10-lequangthuan-htqlntsv <thuanlq200592@sv-onuni.edu.vn>
2026-05-27 23:57:32 +0700

    add file

---
+++ RoomManagement/ViewComponents/UserInfoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoomManagement.Models;
+using System.Security.Claims;
+
+public class UserInfoViewComponent : ViewComponent
+{
+    private readonly RoomManagementContext _context;
+    public UserInfoViewComponent(RoomManagementContext context) => _context = context;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (string.IsNullOrEmpty(userId)) return Content("");
+
+        var user = await _context.Users.FindAsync(long.Parse(userId));
+
+        return View(user);
+    }
+}
